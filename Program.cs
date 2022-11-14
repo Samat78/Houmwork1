3fc4671f35383dd2f4470027cc9d6a9ed7ff15b3
Samat <snabsamatov@gmail.com>
2022-11-14 18:37:34 +0600

    New line1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Введите первое число:");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число:");
+
+// int b = Convert.ToInt32(Console.ReadLine());
+// {
+// Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+// }
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Введите первое число:");
+
+//         int a = Convert.ToInt32(Console.ReadLine());
+
+//         Console.WriteLine("Введите второе число:");
+
+//         int b = Convert.ToInt32(Console.ReadLine());
+
+//         Console.WriteLine("Введите третье число:");
+
+//         int c = Convert.ToInt32(Console.ReadLine());
+//         {
+//             Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+//         }
+//         Console.WriteLine(c > a ? "Третье число больше первого" : "Первое число больше третьего");
+//         {
+//         }
+//     }
+// }
+
