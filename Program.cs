44531754590a030251314c85f72c0bd2be2d6733
Samat <snabsamatov@gmail.com>
2022-11-16 12:58:26 +0600

    Houmwork1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
 // int b = Convert.ToInt32(Console.ReadLine());
 // {
 // Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
-// }
+// } // 1 задание.
 
 // internal class Program
 // {
@@ -31,5 +31,71 @@
 //         {
 //         }
 //     }
+// } // 2 задание
+
+
+
+ 
+// namespace ConsoleApplication1
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int a;                             
+//             Console.WriteLine("введите число: ");
+//             a = Convert.ToInt32(Console.ReadLine()); 
+ 
+//             if (a % 2 == 0)
+//             {
+//                 Console.WriteLine("четное число");
+           
+//             }
+//             else
+//             {
+//                 Console.WriteLine("нечетное");
+//             }
+ 
+ 
+ 
+//             Console.ReadKey();
+//         }
+//     }
 // }
 
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+
+// namespace ConsoleApplication1
+//  {
+//      class Program
+//      {
+//          static void Main(string[] args)
+//          {
+//              int a;                             
+//              Console.WriteLine("введите число: ");
+//              a = Convert.ToInt32(Console.ReadLine()); 
+//              int num = 2;
+// for (int i=0; i<20; i++)
+// {
+//     Console.WriteLine(num);
+//     num = (-(~num)); num = (-(~num));
+// }
+ 
+//              if (a % 2 == 0)
+//              {
+//                  Console.WriteLine("четные числа");
+           
+//              }
+//              else
+//              {
+//                  Console.WriteLine("нечетные числа");
+//              }
+ 
+ 
+ 
+//              Console.ReadKey();
+//          }
+//      }
+//  }
